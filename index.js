6bb8750661517f64750c689eb7b37f496fa6566f
jpwarewal <147260196+jpwarewal@users.noreply.github.com>
2023-10-09 21:34:07 +0530

    code updated

--- index.js
+++ index.js
@@ -1,16 +1,81 @@
 const express = require('express');
+const redis = require('redis');
+
+const client = redis.createClient({
+    url: 'rediss://red-cki1jcsldqrs73f8h1t0:AOLw9I9xkrr7oStAOg8SDO57VC9kThyF@oregon-redis.render.com:6379'
+});
+
+// Connect to Redis
+client.connect();
+
+// Check if the connection is successful
+client.on('ready', () => {
+    console.log('Connected to Redis!');
+    // Once the connection is successful, you can start using Redis commands
+    // For example, to set a value in Redis:
+    // Set a value in Redis with a callback function
+    client.set('my-key', 'my-value', (err, reply) => {
+        if (err) {
+            console.error(err);
+            return;
+        }
+
+        // The value was set successfully
+        console.log(reply);
+    });
+
+    // To get a value from Redis:
+    client.get('my-key', (err, reply) => {
+        if (err) {
+            console.error(err);
+            return;
+        }
+
+        // The value is in the `reply` variable
+        console.log(reply);
+    });
+});
+
+// Handle errors
+client.on('error', (err) => {
+    console.error(err);
+});
+
+
+
 
 const app = express();
 
 // Set the port for the server
-const PORT = 3000;
+const PORT = 3001;
 
 // Create a route for the root URL
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.get('/getdatafromredis', (req, res) => {
+    var query = req.query;
+    var key = query.key;
+    var value = query.value;
+    console.log(query);
+    client.get(key)
+    .then(function(output) {
+        console.log(output);
+        res.send('Hello World!' + output);
+    })
+    
+});
+
+app.get('/setdatainredis', (req, res) => {
+    var query = req.query;
+    var key = query.key;
+    var value = query.value;
+    console.log(query);
+    client.set(key, value)
+    .then(function(res, err){
+        console.log(res);
+    })
+    res.send('Hello World!');
 });
 
 // Start the server
 app.listen(PORT, () => {
-  console.log(`Server running at: http://localhost:${PORT}/`);
+    console.log(`Server running at: http://localhost:${PORT}/`);
 });
